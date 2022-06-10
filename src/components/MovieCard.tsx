50502e2cc145d3cf1019f58750f8a54fa10a32c3
Sipos Bence <sipos.bnc@gmail.com>
2022-06-10 15:27:31 +0200

    movie card details panel wip

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,17 +1,47 @@
-import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
+import {Box, Card, CardActionArea, CardContent, Fade, Icon, Typography} from "@mui/material";
+import {Star} from "@mui/icons-material";
+import {useState} from "react";
 
 export const MovieCard = (props: IMovieCardProps) => {
-  console.log('movieCard props', props);
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
   
   return (
     <Card sx={{width: '100%'}}>
-      <CardActionArea>
+      <CardActionArea onClick={() => setIsDetailsOpen(!isDetailsOpen)}>
         <CardContent>
-          <Typography gutterBottom variant='h5'>
-            {props.name}
+          
+          <Box display='flex'>
+            
+            <Typography gutterBottom variant='h5'>
+              {props.name}
+            </Typography>
+  
+            <Box marginLeft='auto' display='flex' gap='.5rem' alignItems='center'>
+              <Icon>
+                <Star color='warning' />
+              </Icon>
+              
+              <Typography variant='body1' color='text.warning'>
+                {props.score}
+              </Typography>  
+            </Box>
+            
+          </Box>
+          
+          <Typography variant='body2' color='text.secondary'>
+            {props.genres.map(x => x.name).join(', ')}
           </Typography>
+          
         </CardContent>
       </CardActionArea>
+
+      {isDetailsOpen &&
+        <Fade in={isDetailsOpen}>
+          <CardContent>
+            ...details...
+          </CardContent>
+        </Fade>
+      }
     </Card>
   )
 }
